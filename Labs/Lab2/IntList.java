4649632918818b1c644e21c62ca44497f641afc5
Stulionus <mcapcanari@gmail.com>
2023-04-07 15:54:44 -0700

    Lab 2 Point2D part done!

---
+++ Labs/Lab2/IntList.java
@@ -0,0 +1,18 @@
+package Labs.Lab2;
+
+public class IntList { //or IntList, caps at 100 elements with no bullet- proofing or bounds checks, etc.
+        private int numElements = 0;
+        private char[] data = new char[100];
+        public void add(char letter) {
+            data[numElements++] = letter;
+        }
+        public String toString() {
+            String retVal = "";
+            for(int i = 0; i < numElements; i++) {
+                retVal += data[i];
+            }
+            return retVal;
+        }
+//main goes here from the lab
+    }
+
