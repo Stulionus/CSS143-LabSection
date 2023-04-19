2c4ee2289afee99f41935cb3d780cfd3715de38a
Stulionus <mcapcanari@gmail.com>
2023-04-19 13:05:50 -0700

    Lab 3 part 1 in progress

--- Labs/Lab3/Date.java
+++ Labs/Lab3/Date.java
@@ -28,7 +28,31 @@ public class Date {
     public Date(Date other){
         day = other.day;
         month = other.month;
-        year = other.month;
+        year = other.year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     public void setDate(int m, int d, int y){
